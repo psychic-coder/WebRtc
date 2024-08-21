2a5abe7c3865472759003262565157dac8023c28
psychic-coder <rohitganguly450@gmail.com>
2024-08-21 14:16:23 +0530

    made some requieredf changes in the project

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,10 @@
-import { Server } from "socket.io";
-
+const { Server } = require("socket.io");
 
 const io = new Server(8000, {
   cors: true,
 });
 
-//we're are making the socket.id for this email;
 const emailToSocketIdMap = new Map();
-//we're reversing and getting the email id on the basis of the socket
 const socketidToEmailMap = new Map();
 
 io.on("connection", (socket) => {
@@ -16,13 +13,11 @@ io.on("connection", (socket) => {
     const { email, room } = data;
     emailToSocketIdMap.set(email, socket.id);
     socketidToEmailMap.set(socket.id, email);
-    //when the new user tries to koin the room then the users already present in the room gets to know who's joing as they'll will receive the emailid of the new user
     io.to(room).emit("user:joined", { email, id: socket.id });
     socket.join(room);
     io.to(socket.id).emit("room:join", data);
   });
 
-  //we're sending the current socket.id and the offer of the user 
   socket.on("user:call", ({ to, offer }) => {
     io.to(to).emit("incomming:call", { from: socket.id, offer });
   });
@@ -31,7 +26,6 @@ io.on("connection", (socket) => {
     io.to(to).emit("call:accepted", { from: socket.id, ans });
   });
 
-  //with whom we have to negotiate
   socket.on("peer:nego:needed", ({ to, offer }) => {
     console.log("peer:nego:needed", offer);
     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
